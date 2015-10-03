d8946ae61bf5f1aac8fb1175538f65b35c891d65
chenw25 <chenw25@gmail.com>
2015-10-03 17:51:12 -0400

    updated

--- PlayerAI.java
+++ PlayerAI.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.awt.Point;
 
 public class PlayerAI extends ClientAI {
-
 	public PlayerAI() {
 		
 		//Write your initialization here
@@ -10,7 +9,7 @@ public class PlayerAI extends ClientAI {
 
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player) throws NoItemException, MapOutOfBoundsException {
-		
+
 		getClosestPowerUpPath(gameboard,player,gameboard.getPowerUps());
 		//Write your AI here
 		return Move.NONE;
@@ -23,17 +22,68 @@ public class PlayerAI extends ClientAI {
 			return;
 		}
 		
-		Coordinate[] test = new Coordinate[powerUps.size()];
+		Coordinate[] shifts = new Coordinate[powerUps.size()];
 		
-		for (int i = 0; i <test.length;i++){
-			test[i] = getNumTurn(new Coordinate(player.x,player.y), new Coordinate (powerUps.get(i).x,powerUps.get(i).y),gameboard.getWidth(),gameboard.getHeight());
-			System.out.println(test[i].x + "    "+test[i].y);
+		for (int i = 0; i <shifts.length;i++){
+			shifts[i] = getShift(player, new Coordinate (powerUps.get(i).x,powerUps.get(i).y),gameboard.getWidth(),gameboard.getHeight());
+			System.out.println(shifts[i].x + "       "+shifts[i].y);
+		}
+	}
+	
+	private boolean isSafe(Gameboard gameboard, Coordinate destination){
+		int maxWidth = gameboard.getWidth();
+		int maxHeight = gameboard.getHeight();
+		ArrayList<Turret> turrets = gameboard.getTurrets();
+		ArrayList<Bullet> bullets = gameboard.getBullets();
+		
+		ArrayList<Coordinate> markedCoordinate = new ArrayList<Coordinate>();
+		for (Turret turret : turrets){
+			if (isWithinBoundary(gameboard,destination,new Coordinate(turret.x,turret.y))){
+				
+			}
+		}
+		return false;
+	}
+	
+	private boolean isWithinBoundary(Gameboard gameboard,Coordinate player, Coordinate point)
+	{
+		int maxWidth = gameboard.getWidth();
+		int maxHeight = gameboard.getHeight();
+		int lowerX = getRealCoordinate(maxWidth,player.x-5);
+		int upperX = getRealCoordinate(maxWidth,player.x+5);
+		int lowerY = getRealCoordinate(maxHeight, player.y-5);
+		int upperY = getRealCoordinate(maxHeight, player.y+5);
+		boolean checkX = false;
+		boolean checkY = false;
+		//wrap happened for x
+		if (lowerX>upperX){
+			checkX=point.x>=lowerX && point.x<=maxWidth || point.x<upperX;
+		}else{
+			checkX=point.x>=lowerX && point.x<=upperX;
 		}
 		
+		//wrap happened for y
+		if (lowerY>upperY){
+			checkY=point.y>=lowerY && point.y<=maxWidth || point.y<upperY;
+		}else{
+			checkY=point.y>=lowerY && point.y<=upperY;
+		}
+		
+		return checkX && checkY;
 		
 	}
 	
-	private Coordinate getNumTurn(Coordinate player, Coordinate powerUp, int maxWidth, int maxHeight){
+	private int getRealCoordinate(int boundary,int coordinate){
+		if (coordinate<0){
+			return boundary+coordinate;
+		}else if (coordinate>boundary){
+			return coordinate-boundary;
+		}else{
+			return coordinate;
+		}
+	}
+	
+	private Coordinate getShift(Player player, Coordinate powerUp, int maxWidth, int maxHeight){
 		int shiftX = -(player.x - powerUp.x);
 		int shiftWrapX;
 		if (player.x>powerUp.x){
@@ -57,7 +107,18 @@ public class PlayerAI extends ClientAI {
 		if (Math.abs(shiftWrapY)<Math.abs(shiftY)){
 			shiftY = shiftWrapY;
 		}
+		//move right
+		if (shiftX>0 && player.direction != Direction.RIGHT){
+			shiftX++;
+		}else if (shiftX<0 && player.direction != Direction.LEFT){
+			shiftX--;
+		}
 		
+		if (shiftY>0 && player.direction != Direction.DOWN){
+			shiftY++;
+		}else if (shiftY<0 && player.direction != Direction.UP){
+			shiftY--;
+		}
 		return new Coordinate(shiftX,shiftY);
 		
 	}
